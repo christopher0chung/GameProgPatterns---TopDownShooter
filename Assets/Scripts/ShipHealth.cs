fae9511a94bcdf6e3abf509d43889eaf0e95551b
Chris Chung <christopher0chung@gmail.com>
2017-02-26 18:27:09 -0500

    Event system tracking life.
    For some reason, my gitIgnore needed to be pushed again, and Temp was being tracked...

--- Assets/Scripts/ShipHealth.cs
+++ Assets/Scripts/ShipHealth.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ShipHealth : MonoBehaviour, iShootable {
 
-    public int health;
+    [SerializeField] private int health;
+    public delegate void Hit (float _health);
+    public static Hit onHit; 
 
     public void OnShoot(int damage)
     {
         health -= damage;
+        onHit(health);
         if (health <= 0)
         {
-            
+            //explode
         }
     }
 }
